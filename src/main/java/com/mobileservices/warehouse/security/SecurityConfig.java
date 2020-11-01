411244eb39c77bd2564a3dce9c1367a0d6bd11f0
Mile <mile@producement.com>
2020-11-01 23:55:09 +0100

    Reverted cors

--- src/main/java/com/mobileservices/warehouse/security/SecurityConfig.java
+++ src/main/java/com/mobileservices/warehouse/security/SecurityConfig.java
@@ -48,8 +48,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .cors()
-      .and()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and()
       .anonymous()
