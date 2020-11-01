dbfc16331eafa17a8b90f2ff47d045c8c7c5ab8d
Mile <mile@producement.com>
2020-11-01 14:53:22 +0100

    Added the patch mapping for the quantity changing

--- src/main/java/com/mobileservices/warehouse/product/ProductController.java
+++ src/main/java/com/mobileservices/warehouse/product/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -36,16 +37,9 @@ public class ProductController {
     return ResponseEntity.ok(product);
   }
 
-  @PutMapping("/{id}/increaseQuantity")
-  public ResponseEntity<Product> increaseQuantity(@PathVariable("id") long productId, @RequestBody Long quantity) {
+  @PatchMapping("/{id}/quantity")
+  public ResponseEntity<Product> changeQuantity(@PathVariable("id") long productId, @RequestBody Long quantity) {
     final Product product = productService.changeQuantity(quantity, productId);
-    return ResponseEntity.ok(product);
-  }
-
-
-  @PutMapping("/{id}/decreaseQuantity")
-  public ResponseEntity<Product> decreaseQuantity(@PathVariable("id") long productId, @RequestBody Long quantity) {
-    final Product product = productService.changeQuantity(-quantity, productId);
     return ResponseEntity.ok(product);
   }
 
