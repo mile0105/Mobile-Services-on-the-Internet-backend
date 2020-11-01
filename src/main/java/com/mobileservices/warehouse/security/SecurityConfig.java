42fb4818d63e762903e98aa6df922ec6c5240a49
Mile <mile@producement.com>
2020-11-01 23:49:29 +0100

    Added cors to http security

--- src/main/java/com/mobileservices/warehouse/security/SecurityConfig.java
+++ src/main/java/com/mobileservices/warehouse/security/SecurityConfig.java
@@ -48,6 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+      .cors()
+      .and()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and()
       .anonymous()
