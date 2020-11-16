840388c712e93f0ea9108ee869814bb49097b7b5
Mile <mile@producement.com>
2020-11-17 00:26:51 +0100

    Added the empty response when registering

--- src/main/java/com/mobileservices/warehouse/user/UserController.java
+++ src/main/java/com/mobileservices/warehouse/user/UserController.java
@@ -3,6 +3,7 @@ package com.mobileservices.warehouse.user;
 import com.mobileservices.warehouse.security.jwt.CustomJwtToken;
 import com.mobileservices.warehouse.user.model.UserApi;
 import com.mobileservices.warehouse.user.service.UserService;
+import com.mobileservices.warehouse.util.models.EmptyResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +21,10 @@ public class UserController {
   private final UserService userService;
 
   @PostMapping("/register")
-  public ResponseEntity<Void> register(@RequestBody UserApi userApi) {
+  public ResponseEntity<EmptyResponse> register(@RequestBody UserApi userApi) {
 
     userService.register(userApi);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok(new EmptyResponse());
   }
 
   @PostMapping("/register/google")
